5decb19075bd5eac5660c5772f834b1b0b8bbfad
mgarceau <maxwellgarceau@Maxwells-MacBook-Air.local>
2026-08-01 11:06:47 -0400

    exercise 1.1 through 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounces = 10
+
+while bounces > 0:
+    height = height * 3/5
+    print(round(height, 4))
+    bounces = bounces - 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,38 @@
+# # mortgage.py
+# #
+# # Exercise 1.7
+
+# principal = 500000.0
+# rate = 0.05
+# payment = 2684.11
+# total_paid = 0.0
+# extra_payment_start_month = 61
+# extra_payment_end_month = 108
+# extra_payment = 1000
+
+# months = 0
+# while principal > 0:
+#     months = months + 1
+#     payment_modified = payment
+#     if months >= extra_payment_start_month and months <= extra_payment_end_month:
+#         payment_modified = payment + extra_payment
+#     principal = principal * (1+rate/12) - payment_modified
+#     total_paid = total_paid + payment_modified
+
+# print(f'''Total paid: {round(total_paid, 2)}
+# Months: {months}''')
+# # print('Total paid', round(total_paid, 2))
+# # print('Months', months)
+
 # mortgage.py
-#
-# Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', total_paid)
