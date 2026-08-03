7264daff9655e61157a6f2bec3c7e42d109484c1
Max Garceau <maxwellgarceau@gmail.com>
2026-08-03 16:50:07 -0400

    Exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -24,9 +24,41 @@ def read_prices(filename):
     'reads a set of prices such as this into a dictionary where the keys of the dictionary are the stock names and the values in the dictionary are the stock prices'
     f = open(filename, 'r')
     rows = csv.reader(f)
-    price_dict = []
+    price_dict = {}
     for row in rows:
         if row:
-            stock = {str(row[0]): float(row[1])}
-            price_dict.append(stock)
+            price_dict[str(row[0])] = float(row[1])
     return price_dict
+
+# Calculate portfolio total and change in this function
+# The code is messy, but this avoid running multiple loops for a simple task
+def get_change():
+    'Get portfolio total and change for current dates stock prices'
+    # List of stock holdings
+    # Ex: {'name': 'AA', 'price': 32.2, 'shares': 100}
+    portfolio = read_portfolio('Data/portfolio.csv')
+
+    # Stock name -> current price
+    # Ex: {'AA': 20.0}
+    current_prices = read_prices('Data/prices.csv')
+
+    portfolio_total = 0.0
+    change_total = 0.0
+    for holding in portfolio:
+
+        # Calculate stock price change
+
+        # Assuming that all stocks are in current_prices
+        # Otherwise, we would have to add defensive guards here
+        current_price = current_prices[holding['name']]
+        price_change = current_price - holding['price']
+
+        # Sum portfolio total
+        portfolio_total += holding['price'] * holding['shares']
+        change_total += price_change * holding['shares']
+
+    new_total = portfolio_total + change_total
+
+    print('Initial portfolio value: ', f"{portfolio_total:.2f}")
+    print('Total value change: ', f"{change_total:.2f}")
+    print('New portfolio value: ', f"{new_total:.2f}")
