4d62d35470b13a6d060f59285f8062d4bdceb329
Max Garceau <maxwellgarceau@gmail.com>
2026-08-03 15:54:17 -0400

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Opens a given portfolio file and reads it into a list of tuples'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            name = str(row[0])
+            nshares = int(row[1])
+            price = float(row[2])
+            holding = (name, nshares, price)
+            portfolio.append(holding)
+    return portfolio
