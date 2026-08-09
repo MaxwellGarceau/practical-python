d9c9fddea8e98dce9b66e3914e86a3335a90b411
Max Garceau <maxwellgarceau@gmail.com>
2026-08-09 15:37:02 -0400

    Add parse_csv method

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,14 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+from typing import Callable, Any
+
+# Input: rows (comma separated), types, headers
+# Output: List of dictionaries, each dictionary is header -> row value keyed (normalized to type)
+def parse_csv(rows: list[list[str]], headers: list[str], types: list[Callable[..., Any]]) -> list[dict[str, Any]]:
+    data = []
+    for row in rows:
+        record = { name: func(val) for name, func, val in zip(headers, types, row) }
+        data.append(record)
+    return data
