670248eeac14d58d7233e0da7ad6257dd61c59f8
Max Garceau <maxwellgarceau@gmail.com>
2026-08-10 15:44:57 -0400

    Exercise 4.2

--- Work/stock.py
+++ Work/stock.py
@@ -1,5 +1,11 @@
 class Stock:
-    def __init__(self, name, shares, price):
+    def __init__(self, name: str, shares: int, price: float):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def cost(self) -> None:
+        print(self.shares * self.price)
+
+    def sell(self, amount: int) -> None:
+        self.shares = self.shares - amount
