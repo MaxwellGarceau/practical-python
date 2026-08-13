452e810f373829a3a2221100f081b30a87ef3476
Max Garceau <maxwellgarceau@gmail.com>
2026-08-13 11:12:50 -0400

    Exercise 7.10

---
+++ Work/timethis.py
@@ -0,0 +1,10 @@
+from time import time
+
+def timethis(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        r = func(*args, **kwargs)
+        end = time()
+        print('%s.%s: %f' % (func.__module__, func.__name__, end - start))
+        return r
+    return wrapper
