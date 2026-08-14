0f7ec9bc797891f139b9f3ebb470de8d92b3922e
Max Garceau <maxwellgarceau@gmail.com>
2026-08-14 14:51:55 -0400

    Exercise 9.5

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty-max",
+    version="0.0.1",
+    author="Max Garceau",
+    author_email="max@fakeemail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
